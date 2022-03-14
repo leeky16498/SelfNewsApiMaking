3b25e88eed80c03550bf09213e053c417b6504be
Kyungyun Lee, IOS dev <leeky16498@gmail.com>
2022-03-14 15:13:42 +0000

    Initial Commit

---
+++ SelfNewsApiMaking/SelfNewsApiMakingApp.swift
@@ -0,0 +1,17 @@
+//
+//  SelfNewsApiMakingApp.swift
+//  SelfNewsApiMaking
+//
+//  Created by Kyungyun Lee on 14/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct SelfNewsApiMakingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
